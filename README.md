76bb2cacb299bbdcef6c919509453a8035d6949d
Sebass van Boxel <svboxel@gmail.com>
2020-12-16 14:38:29 +0100

    Initial commit from Create Probot App

---
+++ README.md
@@ -0,0 +1,27 @@
+# central-workflows-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run with hot reload
+npm run build:watch
+
+# Compile and run
+npm run build
+npm run start
+```
+
+## Contributing
+
+If you have suggestions for how central-workflows-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2020 Sebass van Boxel <svboxel@gmail.com>
---
+++ test/index.test.ts
@@ -0,0 +1,64 @@
+// You can import your modules
+// import index from '../src/index'
+
+import nock from 'nock'
+// Requiring our app implementation
+import myProbotApp from '../src'
+import { Probot } from 'probot'
+// Requiring our fixtures
+import payload from './fixtures/issues.opened.json'
+const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+const fs = require('fs')
+const path = require('path')
+
+describe('My Probot app', () => {
+  let probot: any
+  let mockCert: string
+
+  beforeAll((done: Function) => {
+    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err: Error, cert: string) => {
+      if (err) return done(err)
+      mockCert = cert
+      done()
+    })
+  })
+
+  beforeEach(() => {
+    nock.disableNetConnect()
+    probot = new Probot({ id: 123, cert: mockCert })
+    // Load our app into probot
+    probot.load(myProbotApp)
+  })
+
+  test('creates a comment when an issue is opened', async (done) => {
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' })
+
+    // Test that a comment is posted
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
+        done(expect(body).toMatchObject(issueCreatedBody))
+        return true
+      })
+      .reply(200)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'issues', payload })
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    nock.enableNetConnect()
+  })
+})
+
+// For more information about testing with Jest see:
+// https://facebook.github.io/jest/
+
+// For more information about using TypeScript in your tests, Jest recommends:
+// https://github.com/kulshekhar/ts-jest
+
+// For more information about testing with Nock see:
+// https://github.com/nock/nock
