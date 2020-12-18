3107abe4a64308e6f84013acb44bc82413704625
Sebass van Boxel <svboxel@gmail.com>
2020-12-18 16:14:19 +0100

    Org level workflow probot app - init commit

---
+++ src/enforce-protection.ts
@@ -0,0 +1,52 @@
+import { Context } from "probot" // eslint-disable-line no-unused-vars
+
+const organization_repository = '.github';
+
+async function enforceProtection( 
+  octokit: Context["octokit"], 
+  repository: { owner: string, repo: string }, 
+  context_name: string, 
+  enforce_admin = false
+) {
+  const repo = await octokit.repos.get({
+    ...repository,
+    mediaType: {
+      previews: ["symmetra"],
+    },
+  });
+  let protection: any = null;
+  try { 
+    protection = await octokit.repos.getBranchProtection({
+      ...repository,
+      branch: repo.data.default_branch
+    });
+  } catch (e) { }
+
+  let enforce_admins = protection && protection.data.enforce_admins.enabled;
+  if (enforce_admins === true) {
+    enforce_admin = true
+  } else if (repository.repo === organization_repository) {
+    enforce_admins = false
+  } else if (enforce_admin) {
+    enforce_admins = enforce_admin
+  }
+  await octokit.repos.updateBranchProtection({
+    ...repository,
+    branch: repo.data.default_branch,
+    required_status_checks: {
+      strict: protection ? protection.data.required_status_checks.strict : false,
+      contexts: [...(protection ? protection.data.required_status_checks.contexts : []), context_name]
+    },
+    enforce_admins,
+    required_pull_request_reviews: protection?.data?.required_pull_request_reviews ? {
+      dismiss_stale_reviews: protection ? protection.data.required_pull_request_reviews.dismiss_stale_reviews : false,
+      require_code_owner_reviews: protection ? protection.data.required_pull_request_reviews.require_code_owner_reviews : 0
+    } : null,
+    required_linear_history: protection && protection.data.required_linear_history.enabled,
+    allow_force_pushes: protection && protection.data.allow_force_pushes.enabled,
+    allow_deletions: protection && protection.data.allow_deletions.enabled,
+    restrictions: null
+  })
+}
+
+export default enforceProtection;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,120 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Probot } from "probot" 
+import { uuid } from 'uuidv4';
+import enforceProtection from './enforce-protection';
 
-export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+const repository_dispatch_type = 'org-workflow-bot';
+const organization_repository = '.github';
+const app_route = "/org-workflows";
+
+let runs: {[key: string]: {
+  id: string;
+  sha: string;
+  callback_url: string;
+  check?: {
+    run_id: number; // The run in the central workflow
+    name?: string; // Name of the status check
+    checks_run_id: number; // ID of status check on commit
+  }
+  repository: {
+    owner: string;
+    name: string;
+    full_name: string;
+  }
+}} = {}
+
+module.exports = (app: Probot, { getRouter }: { getRouter: any }) => {
+  const router = getRouter(app_route);
+
+  app.on('push', async (context) => {
+    const webhook = await context.octokit.apps.getWebhookConfigForApp();
+    
+    const id = uuid();
+    const sha = context.payload.after;
+
+    runs[id] = {
+      id,
+      sha,
+      callback_url: `${webhook.data.url}${app_route}/register`,
+      repository: {
+        owner: context.payload.repository.owner.login,
+        name: context.payload.repository.name,
+        full_name: context.payload.repository.full_name
+      },
+    };
+
+    await context.octokit.repos.createDispatchEvent({
+      owner: context.payload.repository.owner.login,
+      repo: organization_repository,
+      event_type: repository_dispatch_type,
+      client_payload: runs[id]
+    });
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on('workflow_run.completed', async (context) => {
+    if (context.payload.repository.name !== organization_repository) return;
+    
+    const id = Object.keys(runs).find(key => {
+      runs[key].check?.run_id === context?.payload?.workflow_run?.id
+    }) || 0;
+
+    if (!id) return;
+    context.log('finishing check...', id);
+
+    const data: any = {
+      owner: runs[id].repository.owner,
+      repo: runs[id].repository.name,
+      check_run_id: runs[id].check?.checks_run_id,
+      name: `${runs[id].check?.name}`,
+      status: context.payload.workflow_run?.status,
+      conclusion: context.payload.workflow_run?.conclusion
+    }
+
+    await context.octokit.checks.update(data)
+
+    context.log(`${Object.keys(runs).length} runs in memory`);
+  })
+  
+  router.get("/register", async (req: any, res: any) => {
+    console.log(req.query)
+    let octokit = await app.auth()
+    const installation = await octokit.apps.getOrgInstallation({ org: 'moon-organization' })
+    octokit = await app.auth(installation.data.id)
+
+    let id = (req.query.id || "").toString();
+    let run_id = req.query.run_id
+    let name = req.query.name
+    let require = req.query.require === 'true'
+    let enforce_admin = req.query.enforce_admin === 'true'
+
+    console.log('registering check')
+    const checks_run = await octokit.checks.create({
+      owner: runs[id].repository.owner,
+      repo: runs[id].repository.name,
+      head_sha: runs[id].sha,
+      name,
+      details_url: `https://github.com/${runs[id].repository.owner}/${organization_repository}/actions/runs/${run_id}`,
+      status: 'in_progress'
+    })
+
+    if (require) {
+      enforceProtection(
+        octokit, 
+        { owner: runs[id].repository.owner, repo: runs[id].repository.name},
+        name, 
+        enforce_admin
+      ) 
+    }
+
+    runs[id] = {
+      ...runs[id],
+      check: {
+        ...runs[id].check,
+        name,
+        run_id: Number(run_id),
+        checks_run_id: checks_run.data.id
+      }
+    }
+
+    res.sendStatus(200);
+  });
 }
