f5a66e54fcad29fc95e3ab6e30b2ccd445dfbd19
Sebass van Boxel <hello@svboxel.com>
2021-01-11 12:07:36 +0100

    Update e2e-test.js

--- .github/workflows/scripts/e2e-test.js
+++ .github/workflows/scripts/e2e-test.js
@@ -1,4 +1,5 @@
 module.exports = async (APP_ID, TEST_ORG, { github, core }) => {
+  console.log(APP_ID, TEST_ORG);
   const repoName = Math.random().toString(36).substring(7)
   const buffer = new Buffer('Test commit');
   const content = buffer.toString('base64');
