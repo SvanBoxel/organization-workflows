df771be9763dc8e7544365c9af4be2ab6d34fe6a
Sebass van Boxel <svboxel@gmail.com>
2021-01-14 20:53:02 +0100

    fetch config and save in document

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const github_host = 'https://github.com'
-export const organization_repository = '.github'
+export const default_organization_repository = '.github'
 export const app_route = '/org-workflows'
+export const config_keys = ['workflows_repository'] 
