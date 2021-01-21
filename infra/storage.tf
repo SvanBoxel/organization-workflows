58574a6aa8f98a6014cf5296ee2c1ffe783578ba
Sebass van Boxel <svboxel@gmail.com>
2021-01-21 12:18:41 +0100

    Update to tls 1.2

--- infra/storage.tf
+++ infra/storage.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "bot-storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   allow_blob_public_access = true
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "bot-storage-container" {
