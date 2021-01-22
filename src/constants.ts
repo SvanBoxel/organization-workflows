637391f2322c3122d8f564ea53cceabc27310835
Sebass van Boxel <svboxel@gmail.com>
2021-01-22 23:10:08 +0100

    update push handler

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const github_host = 'https://github.com'
 export const default_organization_repository = '.github'
 export const app_route = '/org-workflows'
-export const config_keys = ['workflows_repository'] 
+export const config_keys = ['workflows_repository', 'include', 'exclude'] 
