732988317786f072bed1ebfeeb3c73868ba68704
Thiago Pinto <thspinto@gmail.com>
2021-02-25 18:00:13 -0300

    Add env var for some configs (#31)
    * Add env var for some configs
    
    * Update README.md

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const github_host = 'https://github.com'
-export const default_organization_repository = '.github'
-export const app_route = '/org-workflows'
-export const config_keys = ['workflows_repository'] 
+export const github_host = process.env.GITHUB_HOST ?? 'https://github.com'
+export const default_organization_repository = process.env.DEFAULT_ORGANIZATION_REPOSITORY ?? '.github'
+export const app_route = process.env.APP_ROUTE ?? '/org-workflows'
+export const config_keys = ['workflows_repository']
