6f5e6b61438761b489a6d2ca1147dcd5ec3542b6
Amit Ben Ami <benami.amit1@gmail.com>
2021-04-21 17:37:33 +0300

    Fixing the loading of the env vars

--- src/utils/env-vars.ts
+++ src/utils/env-vars.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 
-let envVars: Map<string, string>;
+let envVars: any;
 
 export const getEnv = (name: string): string | undefined => {
     if (process.env.envVarsJsonFilePath) {
         envVars = JSON.parse(fs.readFileSync(process.env.envVarsJsonFilePath).toString())
     }
 
-    return envVars ? envVars.get(name) ?? process.env[name] : process.env[name];
+    return envVars ? envVars[name] ?? process.env[name] : process.env[name];
 }
