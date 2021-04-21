1ca9d05a1e6da6a6276c8c0a9f04645707cc3d55
Amit Ben Ami <benami.amit1@gmail.com>
2021-04-21 19:29:02 +0300

    Changing the name of the env var

--- src/utils/env-vars.ts
+++ src/utils/env-vars.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 let envVars: any;
 
 export const getEnv = (name: string): string | undefined => {
-    if (process.env.envVarsJsonFilePath) {
-        envVars = JSON.parse(fs.readFileSync(process.env.envVarsJsonFilePath).toString())
+    if (process.env.ENV_VARS_JSON_FILE_PATH) {
+        envVars = JSON.parse(fs.readFileSync(process.env.ENV_VARS_JSON_FILE_PATH).toString())
     }
 
     return envVars ? envVars[name] ?? process.env[name] : process.env[name];
--- test/utils/env-vars.test.ts
+++ test/utils/env-vars.test.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+jest.mock('fs');
 const envVarName = 'TEST_ENV';
 
 describe('env vars from json file', () => {
@@ -19,14 +21,26 @@ describe('env vars from json file', () => {
     })
 
     test('json env var file is found and equal to the regular env vars', async () => {
+        const readFileSyncMock = jest.spyOn(fs, 'readFileSync');
+        readFileSyncMock.mockImplementation(() => {
+            return JSON.stringify({
+                [envVarName]: 'test'
+            })
+        });
+        process.env.ENV_VARS_JSON_FILE_PATH = 'envVars.json';
         const getEnv = await (await import('../../src/utils/env-vars')).getEnv;
-        process.env[envVarName] = 'test';
         expect(getEnv(envVarName)).toEqual('test');
     })
 
     test('json env var file is found and not equal to the regular env vars', async () => {
+        const readFileSyncMock = jest.spyOn(fs, 'readFileSync');
+        readFileSyncMock.mockImplementation(() => {
+            return JSON.stringify({
+                [envVarName]: 'test1'
+            })
+        });
+        process.env.ENV_VARS_JSON_FILE_PATH = 'envVars.json';
         const getEnv = await (await import('../../src/utils/env-vars')).getEnv;
-        process.env[envVarName] = 'test1';
         expect(getEnv(envVarName)).toEqual('test1');
     })
 });
