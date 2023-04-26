2022ddf6d5a5f6ba776c0e293b123b3aa5e6e8f4
Sebass van Boxel <svboxel@gmail.com>
2023-04-26 21:08:57 +0200

    Update cosmos.tf

--- infra/cosmos.tf
+++ infra/cosmos.tf
@@ -8,7 +8,7 @@ resource "azurerm_cosmosdb_account" "bot-cosmos-db" {
   consistency_policy {
     consistency_level       = "Session"
     max_interval_in_seconds = 5
-    max_staleness_prefix    = 100
+    max_staleness_prefix    = 1
   }
 
   capabilities { 
